cd952be538a2c6157b7db2fa612e686eaf9d7fa7
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2021-03-30 11:02:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/cinemaathattakam_chanel")
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/cinemaathattakam_channel")
                     ]
                 ]
             ),
